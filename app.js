60ae209889b1ee37566f571fff8d54c5fe1e2a06
Aryan Rajabi <aryanrajabi1990@gmail.com>
2024-04-02 19:19:52 +0330

    final logics and functions created

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 // http://localhost:3000/items API created by json-server
+
+
 const searchInput = document.getElementById("search");
+const productsDOM = document.querySelector(".products-center");
+const btns = document.querySelectorAll(".btn");
+
+
+
 let allProductsData = [];
 const filters = {
   searchItems: "",
@@ -21,11 +28,29 @@ function renderProducts(_products, _filters) {
   const filteredProducts = _products.filter((p) => {
     return p.title.toLowerCase().includes(_filters.searchItems.toLowerCase());
   });
-  console.log(filteredProducts);
+  productsDOM.innerHTML = "";
   // render to DOM
+  filteredProducts.forEach((item) => {
+    const productDiv = document.createElement("div");
+    productDiv.classList.add("product");
+    productDiv.innerHTML = `<div class="img-container">
+            <img src=${item.image} alt=${item.title} />
+          </div>
+          <div class="product-desc">
+            <p class="product-price">${item.price} $</p>
+            <p class="product-title">${item.title}</p>
+          </div>`;
+    productsDOM.appendChild(productDiv);
+  });
 }
 searchInput.addEventListener("input", (e) => {
-  console.log(e.target.value);
   filters.searchItems = e.target.value;
   renderProducts(allProductsData, filters);
+});
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    filters.searchItems = e.target.dataset.filter;
+    renderProducts(allProductsData, filters);
+  });
 });
